fa7760afc915d7c2e6b7a2cb1287498c1f17985b
akhilesh6878 <akhileshmalviya44@gmail.com>
2019-03-12 13:26:46 +0530

    First commit

---
+++ public_html/config.php
@@ -0,0 +1,55 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+
+
+//$sql_db_host = "35.200.236.249";
+
+
+
+// MySQL Database User
+
+
+//$sql_db_user = "webdev";
+// MySQL Database Password
+
+
+//$sql_db_pass = "start123msx";
+
+
+// MySQL Database Name
+
+//$sql_db_name = "webdev_mitrah";
+
+
+// Site URL
+//$site_url = "https://mitrah.in"; // e.g (http://example.com)
+
+
+//$sqlConnect =  mysqli_connect($sql_db_host,$sql_db_user,$sql_db_pass,$sql_db_name);
+
+
+
+
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "webdev_mitrah";
+// MySQL Database Password
+$sql_db_pass = "Start@123iris";
+// MySQL Database Name
+$sql_db_name = "webdev_mitrah";
+// Site URL
+$site_url = "https://mitrah.in";
+
+
+// Purchase code
+$purchase_code = "952ed053-3362-40d3-8018-efb72b66731b"; // Your purchase code, don't give it to anyone. 
+?>
---
+++ public_html/config_old.php
@@ -0,0 +1,24 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "webdev_mitrah";
+// MySQL Database Password
+$sql_db_pass = "zPenEF4^C.M#";
+// MySQL Database Name
+$sql_db_name = "webdev_mitrah";
+// Site URL
+$site_url = "http://mitrah.in"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "952ed053-3362-40d3-8018-efb72b66731b"; // Your purchase code, don't give it to anyone. 
+?>
